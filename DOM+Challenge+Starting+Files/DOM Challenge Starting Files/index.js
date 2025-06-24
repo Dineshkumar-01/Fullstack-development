ac37ea8ce1b953bd4595ef10ba026ab98116120b
Dineshkumar <dinezkumar08@gmail.com>
2025-06-24 21:01:56 +0530

    javascript projects and dice uploaded

---
+++ DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -0,0 +1,5 @@
+document.querySelector(".list a").style.color="red";
+document.querySelector("h1").innerHTML="good";
+document.querySelectorAll(".list")[2].innerHTML="angela";
+document.querySelector("body button").style.backgroundColor="yellow";
+document.querySelector("h1").classList.add("huge");
