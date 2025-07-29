df7e085c6dd6e10f37dbbbdc3ab48627853dc38e
Dineshkumar <dinezkumar08@gmail.com>
2025-07-29 21:29:23 +0530

    EJS partials are completed successfully

---
+++ 4.3+EJS+Partials/4.3 EJS Partials/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+/* Write your code here:
+Step 1: Render the home page "/" index.ejs
+Step 2: Make sure that static files are linked to and the CSS shows up.
+Step 3: Add the routes to handle the render of the about and contact pages.
+  Hint: Check the nav bar in the header.ejs to see the button hrefs
+Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
+
+app.get("/",(req,res)=>{
+  app.use(express.static("public"));
+  res.render("D:/Fullstack course/4.3+EJS+Partials/4.3 EJS Partials/views/index.ejs");
+});
+app.get("/about",(req,res)=>{
+  app.use(express.static("public"));
+  res.render("D:/Fullstack course/4.3+EJS+Partials/4.3 EJS Partials/views/about.ejs");
+})
+app.get("/contact",(req,res)=>{
+  app.use(express.static("public"));
+  res.render("D:/Fullstack course/4.3+EJS+Partials/4.3 EJS Partials/views/contact.ejs")
+})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
